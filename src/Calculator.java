64086006696b7125ed64e5b7bdf3529aec6b6e74
Thomas1023 <thomas.steiner03@gmail.com>
2018-04-25 17:29:52 -0700

    Jtext to JLabel
    s

--- src/Calculator.java
+++ src/Calculator.java
@@ -9,31 +9,27 @@ public class Calculator extends java.applet.Applet
     TextField theDisplay = new TextField();
     public void init() {
         setFont( new Font("Monospaced", Font.BOLD, 24) );
-        addContainerListener( this );
         gbc.gridwidth=4;
         addGB( this, theDisplay, 0, 0 );
         // make the top row
         Panel topRow = new Panel(); 
-        topRow.addContainerListener( this );
+
         gbc.gridwidth = 1;
         gbc.weightx = 1.0;
-        addGB( topRow, new Button("C"), 0, 0 );
+        addGB( topRow, new Button("Put"), 0, 0 );
         gbc.weightx = 0.33;
-        addGB( topRow, new Button("%"), 1, 0 );
+        addGB( topRow, new Button("what"), 1, 0 );
         gbc.weightx = 1.0;
-        addGB( topRow, new Button("+"), 2, 0 );
+        addGB( topRow, new Button("ever"), 2, 0 );
         gbc.gridwidth = 4;
         addGB( this, topRow, 0, 1 );
         gbc.weightx = 1.0;  gbc.gridwidth = 1;
-        // make the digits
         for(int j=0; j<3; j++)
             for(int i=0; i<3; i++)
                 addGB( this, new Button( "" + ((2-j)*3+i+1) ), i, j+2 );
-        // -, x, and divide
-        addGB( this, new Button("-"), 3, 2 );
-        addGB( this, new Button("x"), 3, 3 );
-        addGB( this, new Button("\u00F7"), 3, 4 );
-        // make the bottom row
+        addGB( this, new Button("HI"), 3, 2 );
+        addGB( this, new Button("LUC"), 3, 3 );
+        addGB( this, new Button("KY"), 3, 4 );
         Panel bottomRow = new Panel(); 
         bottomRow.addContainerListener( this );
         gbc.weightx = 1.0;
@@ -51,16 +47,21 @@ public class Calculator extends java.applet.Applet
         gbc.gridx = x;  gbc.gridy = y;
         cont.add( comp, gbc );
     }
-    public void componentAdded( ContainerEvent e ) {
-        Component comp = e.getChild();
-        if ( comp instanceof Button )
-            ((Button)comp).addActionListener( this );
-    }
-    public void componentRemoved( ContainerEvent e ) { }
-    public void actionPerformed( ActionEvent e ) {
-        if ( e.getActionCommand().equals("C") )
-            theDisplay.setText( "" );
-        else 
-            theDisplay.setText( theDisplay.getText() + e.getActionCommand() );
-    }
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void componentAdded(ContainerEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void componentRemoved(ContainerEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+  
+  
 }
